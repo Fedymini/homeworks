d825601395960b80ba3927581bebe866649a6e6f
Fedymini <oppogodin@gmail.com>
2025-05-24 15:36:04 +0300

    Исправление задания 1

--- task.js
+++ task.js
@@ -2,14 +2,15 @@ function getArrayParams(...arr) {
 	let min = Infinity;
 	let max = -Infinity;
 	let sum = 0;
-}
-for (let i = 0; i < arr.length; i++) {
-	sum += arr[i];
-	if (arr[i] < min) {
-		min = arr[i];
-	}
-	if (arr[i] > max) {
-		max = arr[i];
+
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		if (arr[i] > max) {
+			max = arr[i];
+		}
 	}
 
 	let avg = sum / arr.length;
